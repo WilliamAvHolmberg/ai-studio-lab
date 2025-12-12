c69dbcb92ae76db22b8ffdd17861a9ac51562f12
William Holmberg <william.av.holmberg@gmail.com>
2025-12-12 11:31:20 +0100

    bug:bug

--- web/App.tsx
+++ web/App.tsx
@@ -75,7 +75,7 @@ const App: React.FC = () => {
     <Layout 
       currentPhase={projectState.currentPhase} 
       onPhaseChange={handlePhaseChange}
-      projectName={projectState.name}
+      projectName={projectState.name + " (but broken lol)"}
     >
       {renderPhase()}
     </Layout>
